75eb5bf1c1cc79634fba61981a0de2967bc1da93
quocduong <baoduong2972003@gmail.com>
2025-05-13 23:22:39 +0700

    test-backend

--- src/middlewares/asyncWrapper.js
+++ src/middlewares/asyncWrapper.js
@@ -1,5 +1,4 @@
-// src/middlewares/asyncWrapper.js
-// Giúp không cần viết try...catch trong mỗi async controller
+
 const asyncWrapper = (fn) => {
     return (req, res, next) => {
         fn(req, res, next).catch(next); // Lỗi sẽ được chuyển tới global error handler
